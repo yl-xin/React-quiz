1708b6962042c408cd8fa737d6a3874442c4a1d7
Yanlian Xin <xinyanlian@outlook.com>
2020-03-20 13:00:53 -0400

    nextQuestion

--- src/components/QuizDisplay.js
+++ src/components/QuizDisplay.js
@@ -3,34 +3,63 @@ import {quesData} from './quesData';
 
 class QuizDisplay extends React.Component {
     state = {
-        currentQues: 0, userAnswer: null,
-        question: '', choices: [],answer:0
+        currentQues: 0, userAnswer: null, btnDisabled:true, correct:0,
+        question: '', choices: [],answer:''
     };
+    // initialize state 
     componentDidMount(){
         this.setState({
             question:quesData[this.state.currentQues].ques,
             choices:quesData[this.state.currentQues].choices,
             answer:quesData[this.state.currentQues].answ
-        });
-         
+        });   
     };
- 
+    // update state
+    componentDidUpdate(prevProps, prevState){
+        if (this.state.currentQues !== prevState.currentQues) {
+            this.setState({
+                btnDisabled:true,
+                question:quesData[this.state.currentQues].ques,
+                choices:quesData[this.state.currentQues].choices,
+                answer:quesData[this.state.currentQues].answ
+            });
+          }
+    }
+
+
+    selectAnswer = choice=>{
+        this.setState({userAnswer:choice,btnDisabled:false})
+    }
+    nextQuestion= ()=>{
+        // const userAnswer =this.state.userAnswer;
+        // const answer = this.state.answer;
+        // const correct = this.state.correct;
+        const {userAnswer,answer,correct,currentQues} =this.state;
+        if (userAnswer===answer){
+            this.setState({correct:correct+1});
+        }
+        this.setState({currentQues:currentQues+1});
+    }
  
     render() {
         const userAnswer = this.state.userAnswer;
         return (
             <div>
                 <form >
-                    <div>{this.state.question} </div>
+                    <h3>{this.state.question} </h3>
                     { this.state.choices.map((choice)=>{return (
                         <div key={choice.charAt(0)} 
+                        onClick={()=>this.selectAnswer(choice)}
                         className={`ui floating message ${userAnswer=== choice ? "selected" : null}`}
                         >{choice}</div>
                         )
                    })}
                 </form>
-                <div className="small ui animated button" tabIndex="0" type="button" value="Next"
-                    >
+                <div disabled={this.state.btnDisabled} 
+                    className="small ui animated button" tabIndex="0" type="button" value="Next"
+                    onClick={this.nextQuestion}
+
+                >
                     <div className="visible content">Next</div>
                     <div className="hidden content">
                         <i className="right arrow icon"></i>
--- src/components/quesData.js
+++ src/components/quesData.js
@@ -2,16 +2,16 @@ export const quesData = [
     {   id:0,
         ques: "What are Canada's official languages?",
         choices: ["A. English and Acadian", "B. French, English and Michif", "C. English and French", "D. French, English and Inuktitut"],
-        answ: 2
+        answ:"C. English and French"
     },
     {   id:1,
         ques: "What's the only official bilingual province in Canada?",
         choices: ["A. Quebec", "B. Ontari", "C. New Brunswic", "D. Manitoba"],
-        answ: 2
+        answ: "C. New Brunswic"
     },
     {   id:2,
         ques: "Which province has its own time zone?",
         choices: ["A. British Columbia", "B. Newfoundland and Labrador", "C. Nova Scotia", "D. Nunavut"],
-        answ: 1
+        answ: "B. Newfoundland and Labrador"
     }
 ]
