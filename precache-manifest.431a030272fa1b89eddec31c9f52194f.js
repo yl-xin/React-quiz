def353bee6019ef3f187ba1cb60a00290372d11b
Yanlian Xin <xinyanlian@outlook.com>
2020-03-23 18:01:43 -0400

    Updates

---
+++ precache-manifest.431a030272fa1b89eddec31c9f52194f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb1f2bf674bc67f9b1d5b0d45fbebc4",
+    "url": "/React-quiz/index.html"
+  },
+  {
+    "revision": "451ae354e4355e782e3b",
+    "url": "/React-quiz/static/css/main.eac15407.chunk.css"
+  },
+  {
+    "revision": "6b4f8a4b22e51f22d298",
+    "url": "/React-quiz/static/js/2.2547c879.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-quiz/static/js/2.2547c879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "451ae354e4355e782e3b",
+    "url": "/React-quiz/static/js/main.bade709b.chunk.js"
+  },
+  {
+    "revision": "3be25cc18c7247a1bf36",
+    "url": "/React-quiz/static/js/runtime-main.b4a9a91d.js"
+  },
+  {
+    "revision": "4f80ed3f5c268f7115fe259a2300bb01",
+    "url": "/React-quiz/static/media/canada0.4f80ed3f.jpg"
+  },
+  {
+    "revision": "67f066a0dd3816ee637f5654b500d3ea",
+    "url": "/React-quiz/static/media/lose.67f066a0.gif"
+  },
+  {
+    "revision": "2bee0975deb574e65c705975481830a6",
+    "url": "/React-quiz/static/media/meh.2bee0975.gif"
+  },
+  {
+    "revision": "f88a476813b4a8c4b48f620a9f297e38",
+    "url": "/React-quiz/static/media/win.f88a4768.gif"
+  }
+]);
